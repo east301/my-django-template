c71bddf8429e9a67d2691ce7c44532d646615bbd
east301 <me@east301.net>
2014-03-24 17:41:47 +0900

    add missing app to INSTALLED_APPS

--- src/project_name/settings.py
+++ src/project_name/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.lib',
 ) + ADDITIONAL_INSTALLED_APPS
 
 MIDDLEWARE_CLASSES = (
