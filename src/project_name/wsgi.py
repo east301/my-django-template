e5ff86cbe34a2d0c2d80ad48e9134ce6f377d2a8
east301 <me@east301.net>
2014-03-28 21:25:50 +0900

    fix python path in wsgi.py, and change to load virtualenv in wsgi.py

--- src/project_name/wsgi.py
+++ src/project_name/wsgi.py
@@ -7,9 +7,33 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+# ==================================================
+# Sets python path, and change current directory
+# ==================================================
+
 import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, BASE_DIR)
+os.chdir(BASE_DIR)
+
+# ==================================================
+# Loads environment
+# ==================================================
+
+VENV_DIR = os.environ.get('VENV_DIR', os.path.join(BASE_DIR, 'venv'))
+ACTIVATOR = os.path.join(VENV_DIR, 'bin', 'activate_this.py')
+
+if os.path.exists(ACTIVATOR):
+    execfile(ACTIVATOR, {'__file__': ACTIVATOR})
+
+# ==================================================
+# Loads WSGI application
+# ==================================================
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
