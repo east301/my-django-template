fcb84e13029ea3578bf813c9ef6bd3aea4942ed4
east301 <me@east301.net>
2014-03-28 11:50:26 +0900

    read environment variables from env.ini

--- src/project_name/settings_production.py
+++ src/project_name/settings_production.py
@@ -2,9 +2,30 @@
 Django settings for {{ project_name }} project.
 """
 
+from __future__ import with_statement
+import ConfigParser
 import os
 import dj_database_url
 import django_cache_url
+
+
+# ==================================================
+# Environment variables
+# ==================================================
+
+my_dir_path = os.path.dirname(os.path.abspath(__file__))
+env_ini_path = os.path.join(my_dir_path, 'env.ini')
+
+if os.path.exists(env_ini_path):
+    cp = ConfigParser.SafeConfigParser()
+    cp.optionxform = str
+
+    with open(env_ini_path) as fin:
+        cp.readfp(fin)
+
+    if cp.has_section('environment'):
+        for key, value in cp.items('environment'):
+            os.environ[key] = value
 
 
 # ==================================================
