09e9ec68edc721951c3eb734978a85aa0cbcf1c9
Shu Tadaka <tadaka@sb.ecei.tohoku.ac.jp>
2014-05-24 11:15:57 +0900

    update

--- src/project_name/settings_production.py
+++ src/project_name/settings_production.py
@@ -3,77 +3,63 @@ Django settings for {{ project_name }} project.
 """
 
 from __future__ import with_statement
-import ConfigParser
-import os
-import dj_database_url
-import django_cache_url
 
-
-# ==================================================
-# Environment variables
-# ==================================================
-
-my_dir_path = os.path.dirname(os.path.abspath(__file__))
-env_ini_path = os.path.join(my_dir_path, 'env.ini')
-
-if os.path.exists(env_ini_path):
-    cp = ConfigParser.SafeConfigParser()
-    cp.optionxform = str
-
-    with open(env_ini_path) as fin:
-        cp.readfp(fin)
-
-    if cp.has_section('environment'):
-        for key, value in cp.items('environment'):
-            os.environ[key] = value
-
-
-# ==================================================
+# ================================================================================
 # Secret key
-# ==================================================
+# ================================================================================
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = None
 
 
-# ==================================================
+# ================================================================================
 # Debug flag
-# ==================================================
+# ================================================================================
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
-# ==================================================
+# ================================================================================
 # Django applications / middlewares
-# ==================================================
+# ================================================================================
 
 ADDITIONAL_INSTALLED_APPS = (
 )
 
 
-# ==================================================
+# ================================================================================
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-# ==================================================
+# ================================================================================
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': None,
+        'NAME': None,
+        'USER': None,
+        'PASSWORD': None,
+        'HOST': None,
+        'PORT': None,
+    }
 }
 
 
-# ==================================================
+# ================================================================================
 # Cache
 # https://docs.djangoproject.com/en/1.6/ref/settings/#caches
-# ==================================================
+# ================================================================================
 
 CACHES = {
-    'default': django_cache_url.config()
+    'default': {
+        'BACKEND': None,
+    }
 }
 
-# ==================================================
+
+# ================================================================================
 # Mail
 # https://docs.djangoproject.com/en/1.6/topics/email/
-# ==================================================
+# ================================================================================
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- src/project_name/wsgi.py
+++ src/project_name/wsgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-# ==================================================
+# ================================================================================
 # Sets python path, and change current directory
-# ==================================================
+# ================================================================================
 
 import os
 import sys
@@ -19,19 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_DIR)
 os.chdir(BASE_DIR)
 
-# ==================================================
-# Loads environment
-# ==================================================
 
-VENV_DIR = os.environ.get('VENV_DIR', os.path.join(BASE_DIR, 'venv'))
-ACTIVATOR = os.path.join(VENV_DIR, 'bin', 'activate_this.py')
-
-if os.path.exists(ACTIVATOR):
-    execfile(ACTIVATOR, {'__file__': ACTIVATOR})
-
-# ==================================================
+# ================================================================================
 # Loads WSGI application
-# ==================================================
+# ================================================================================
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
 
